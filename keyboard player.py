c1dc70839c44146583acfe082cad6b83a34c0ae4
vasanth kumar(maan petter) <48177734+vasanthkumar7@users.noreply.github.com>
2020-05-28 09:56:22 +0530

    Update keyboard player.py

--- keyboard player.py
+++ keyboard player.py
@@ -3,56 +3,60 @@ import time
 #tamilsongs
 ponepo="CBC CBC ACB ABG CBC CBC ACB ABG CBC CBC ACB ABG CBCDC CBC ACB ABG  C G A C G A CCBC CCBC AA CB AB CCBC CCBC AA CB AB E EA GGFG FFEF DF E E EA GGFG FFEF DF E"
 takita="D#DCG D#DCG D#DCG D#DCG D#DCG D#GD#D CD#D CD#D CD#D CD#D C  D D# C  D D#  "
-happy="AA BADC# AA BAED AA A F# D C# B GG F# D E D AA B A D C# AA B A E D AA B F# D C# B GG F# D E D "
-
+happy="G G A G C B G G A G D C G G G E C B A F F E C D C GGAGCB GGAGDC GGGECBA FFECDC GGAGCB GGAGDC GGGECBA FFECDC "
+shape="D F D D F D D F D A A A Bb Bb Bb Bb Bb Bb D D D G G G Bb Bb Bb "
+cofin="A#A#A#A#DDDDCCCCFFFFGGGGGGGGGGGGcA#AFGGDCA#AAACA#AAA#GGA#AA#AA#GGDCA#AAACA#AGGA#AA#A#BBA#AA#AA#GGDCA#AAACA#AGGA#AA#A#BBA#AA#AA#A#A#A#A#A#DDDDCCCCFFFFGGGGGGGGGGGGCA#AFGGDCA#AAACA#AGG "
 keyboard=Controller()
 time.sleep(2)
-notes=happy
+notes=cofin
 for i in range(len(notes)):
+    if notes[i]=='b' and notes[i+1]!='#':
+        keyboard.press('v')
+        keyboard.release('v')
     if notes[i]=='C' and notes[i+1]!='#':
-        keyboard.press('i')
-        keyboard.release('i')
+        keyboard.press('b')
+        keyboard.release('b')
     if notes[i]=='D' and notes[i+1]!='#':
-        keyboard.press('o')
-        keyboard.release('o')
+        keyboard.press('n')
+        keyboard.release('n')
     if notes[i]=='F' and notes[i+1]!='#':
-        keyboard.press('r')
-        keyboard.release('r')
+        keyboard.press(',')
+        keyboard.release(',')
     if notes[i]=='E' :
-        keyboard.press('p')
-        keyboard.release('p')
+        keyboard.press('m')
+        keyboard.release('m')
     if notes[i]=='B' and notes[i+1]!='#':
-        keyboard.press('u')
-        keyboard.release('u')
+        keyboard.press('v')
+        keyboard.release('v')
     if notes[i]=='A' and notes[i+1]!='#':
-        keyboard.press('y')
-        keyboard.release('y')
+        keyboard.press('c')
+        keyboard.release('c')
     if notes[i]=='G' and notes[i+1]!='#':
-        keyboard.press('t')
-        keyboard.release('t')
+        keyboard.press('x')
+        keyboard.release('x')
     if notes[i]=='C' and notes[i+1]=='#':
-        keyboard.press('9')
-        keyboard.release('9')
+        keyboard.press('h')
+        keyboard.release('h')
         i=i+1
     if notes[i]=='D' and notes[i+1]=='#':
-        keyboard.press('0')
-        keyboard.release('0')
+        keyboard.press('j')
+        keyboard.release('j')
         i=i+1
     if notes[i]=='F' and notes[i+1]=='#':
-        keyboard.press('5')
-        keyboard.release('5')
+        keyboard.press('l')
+        keyboard.release('l')
         i=i+1
    
    
     if notes[i]=='A' and notes[i+1]=='#':
-        keyboard.press('7')
-        keyboard.release('7')
-        i=i+1
-    if notes[i]=='G' and notes[i+1]=='#':
         keyboard.press('f')
         keyboard.release('f')
         i=i+1
+    if notes[i]=='G' and notes[i+1]=='#':
+        keyboard.press(';')
+        keyboard.release(';')
+        i=i+1
         
     
-    time.sleep(0.30)
+    time.sleep(0.20)
     
